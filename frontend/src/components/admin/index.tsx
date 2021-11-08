7e824be0b3f24a8c9a8c8ceb6d8fff25a1e8735f
Nguyen Trung <trungdipdo5@gmail.com>
2021-11-08 17:26:35 +0700

    update user page and admin page

--- frontend/src/components/admin/index.tsx
+++ frontend/src/components/admin/index.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { useToasts } from "react-toast-notifications";
 import environment from "../../config";
 import { UserContext } from "../../contexts/user-reducer";
@@ -29,6 +30,9 @@ export const Admin = () => {
     return (
         <div className="flex justify-center items-center">
             <div className="max-w-3xl">
+                <div className="p-2 text-center">
+                    <Link to="/create">Create new class</Link>
+                </div>
                 <div className="grid grid-cols-10 p-2 border-b-2 border-blue-400">
                     <div className="p-2 col-span-2 text-center">
                         <p>Class id</p>
